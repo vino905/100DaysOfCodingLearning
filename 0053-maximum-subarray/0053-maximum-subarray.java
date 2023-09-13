8824537b3e55a13468a22149dc8422348eddd449
Vinod Kalwani <71549913+vino905@users.noreply.github.com>
2023-09-13 23:09:20 +0530

    Time: 1 ms (100%), Space: 59.6 MB (41.64%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=Integer.MIN_VALUE;  
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(sum<0){
+                sum=0;
+            }
+            sum+=nums[i];
+            max=Math.max(sum,max);
+        }
+    return max;    
+    }
+}
