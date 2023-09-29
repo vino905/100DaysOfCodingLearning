b9607638f7ea5e2cfd756fc7a9e1f2cf6f3667e1
Vinod Kalwani <71549913+vino905@users.noreply.github.com>
2023-09-29 19:31:24 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Anagram/anagram.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args)throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0)
+		{
+		    String str = br.readLine();
+		    String s1 = str.split(" ")[0];
+		    String s2 = str.split(" ")[1];
+		    
+		    Solution obj = new Solution();
+		    
+		    if(obj.isAnagram(s1,s2))
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		         System.out.println("NO");
+		    }
+		    
+		    
+		    
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{    
+    //Function is to check whether two strings are anagram of each other or not.
+    public static boolean isAnagram(String a,String b)
+    {
+        char[] str1= a.toCharArray();
+        char[] str2= b.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        
+        return (new String (str1)).equals(new String (str2)) ? true : false;
+    }
+}
