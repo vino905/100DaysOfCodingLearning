7aef9f113c684a8a3e6f1d494c1155a52cd7ce41
Vinod Kalwani <VINODKALWANI2001@GMAIL.COM>
2024-10-06 12:55:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+
+        // looping through all testcases
+        while (testcases-- > 0) {
+            String line = br.readLine();
+            String[] element = line.trim().split("\\s+");
+            int sizeOfArray = Integer.parseInt(element[0]);
+            int K = Integer.parseInt(element[1]);
+
+            int arr[] = new int[sizeOfArray];
+
+            line = br.readLine();
+            String[] elements = line.trim().split("\\s+");
+            for (int i = 0; i < sizeOfArray; i++) {
+                arr[i] = Integer.parseInt(elements[i]);
+            }
+
+            Solution obj = new Solution();
+            int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr(int A[], int N, int k) {
+        Map<Integer,Integer> preSumMap = new HashMap<>();
+        
+        int resLen = 0;
+        int sum =0;
+        for(int i=0;i<N;i++){
+            sum+=A[i];
+            
+            if(sum==k){
+                resLen = Math.max(resLen,i+1);
+            }
+            
+            int rem = sum-k;
+            
+            if(preSumMap.containsKey(rem)){
+                int len = i-preSumMap.get(rem);
+                resLen = Math.max(len,resLen);
+            }
+            
+            if(!preSumMap.containsKey(sum)){
+                preSumMap.put(sum,i);
+            }
+        }
+    return resLen;    
+    }
+}
