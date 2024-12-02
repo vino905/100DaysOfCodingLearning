7a23a869b6ef035d04f4097a17da495b3e813fa3
Vinod Kalwani <VINODKALWANI2001@GMAIL.COM>
2024-12-02 22:10:22 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (14.15%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,12 +11,13 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
         ListNode slow = head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-            if(fast==slow){
+        ListNode fast = head;
+        
+        while(fast!= null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
                 return true;
             }
         }
