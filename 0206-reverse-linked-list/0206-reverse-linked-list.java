b840643ec58a414729ed71899688d87efc17a7ed
Vinod Kalwani <VINODKALWANI2001@GMAIL.COM>
2024-12-14 18:01:57 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (25.80%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,21 +10,42 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head==null){
-            return head;
-        }  
-        ListNode prev= null;
-        ListNode present=head;
-        ListNode next=present.next;
+        
+        // Brute force using stack with tc o(N) and space complexity o(N)
+//         Stack<Integer> values = new Stack<>();
+        
+//         ListNode temp = head;
+        
+//         while(temp!=null){
+//             values.push(temp.val);
+//             temp = temp.next;
+//         }
+//         temp = head;
 
-        while(present!=null){
-            present.next=prev;
-            prev=present;
-            present=next;
-            if(next!=null){
-                next=next.next;
-            }
+        
+//         while(temp!=null){
+//             temp.val = values.pop();
+//             temp = temp.next;
+//         }
+//         return head;
+        
+        // optimized tc o(N) and sc o(N) 
+        
+        // but it also possible that ll have only one node
+        
+        if( head == null || head.next == null){
+            return head;
         }
-    return prev;    
+        ListNode temp = head;
+        ListNode prev = null;
+        
+        while(temp!=null){
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        
+        return prev;
     }
 }
