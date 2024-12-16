28f99192be833381bbc37fbd25fe9f38dc9904fa
Vinod Kalwani <VINODKALWANI2001@GMAIL.COM>
2024-12-16 14:44:01 +0530

    Time: 14 ms (15.70%), Space: 58.3 MB (77.82%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,50 +9,25 @@
  * }
  */
 class Solution {
-    public ListNode reverselist(ListNode head){
-        if(head==null){
-            return head;
-        }
-        ListNode prev = null;
-        ListNode next = head.next;
-
-        while(head!=null){
-               head.next=prev;
-               prev=head;
-               head=next;
-               if(next!=null){
-                   next=next.next;
-               }
-        }
-    return prev;    
-    }
-    public ListNode middleNode(ListNode head) {
-        ListNode fast=head;
-        ListNode  slow=head;
-        while(fast!=null && fast.next!=null){
-           slow=slow.next;
-           fast=fast.next.next;
-        
-        }
-    return slow;
-    }
     public boolean isPalindrome(ListNode head) {
         
-        ListNode mid=middleNode(head);
-        ListNode secondNode=reverselist(mid);
-        ListNode reversNode=secondNode;
-
-        while(head!=null && secondNode!=null){
-            if(head.val!=secondNode.val){
-                break;
+        // brute force using stack . with tc o 2 (N) and sc o(N)
+        
+        Stack<Integer> st = new Stack<>();
+        
+        ListNode temp = head;
+        while(temp!=null){
+            st.push(temp.val);
+            temp = temp.next;
+        }
+        
+        temp = head;
+        while(temp!=null){
+            if(temp.val!=st.pop()){
+                return false;
             }
-            head=head.next;
-            secondNode=secondNode.next;
+            temp = temp.next;
         }
-        reverselist(reversNode);
-        if(head==null || secondNode==null){
-            return true;
-        }
-        return false;
+    return true;    
     }
 }
