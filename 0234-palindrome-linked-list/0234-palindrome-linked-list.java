252d80b09d4dc2e72e43326c4dfe17a4f9a2a4b6
Vinod Kalwani <VINODKALWANI2001@GMAIL.COM>
2024-12-16 15:02:30 +0530

    Time: 9 ms (26.57%), Space: 72.6 MB (5.30%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,25 +9,68 @@
  * }
  */
 class Solution {
+    public ListNode reverseLL(ListNode head){
+        if(head ==null || head.next == null ){
+            return head;
+        }
+        
+        ListNode newHead = reverseLL(head.next);
+        
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
+    }
     public boolean isPalindrome(ListNode head) {
         
         // brute force using stack . with tc o 2 (N) and sc o(N)
         
-        Stack<Integer> st = new Stack<>();
+//         Stack<Integer> st = new Stack<>();
         
-        ListNode temp = head;
-        while(temp!=null){
-            st.push(temp.val);
-            temp = temp.next;
+//         ListNode temp = head;
+//         while(temp!=null){
+//             st.push(temp.val);
+//             temp = temp.next;
+//         }
+        
+//         temp = head;
+//         while(temp!=null){
+//             if(temp.val!=st.pop()){
+//                 return false;
+//             }
+//             temp = temp.next;
+//         }
+//     return true;    
+        
+        // But we have to optimized the code and we don't have to use extra space with SC O(1) a
+        //     and TC O 2(N) 
+        
+        if(head ==null || head.next == null ){
+            return true;
         }
         
-        temp = head;
-        while(temp!=null){
-            if(temp.val!=st.pop()){
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode newHead = reverseLL(slow.next);
+        
+        ListNode first = head;
+        ListNode second = newHead;
+        
+        while(second!=null){
+            if(second.val!=first.val){
+                reverseLL(newHead);
                 return false;
             }
-            temp = temp.next;
+            first = first.next;
+            second = second.next;
         }
-    return true;    
+     reverseLL(newHead);
+     return true;   
     }
 }
